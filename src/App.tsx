dc2de1a53a78c568a13f838b9ffe89c84471ecce
JJongsKim <totnwjd530@gmail.com>
2024-06-16 12:53:57 +0900

    Style: globalStyle 변경 및 App 컴포넌트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/globalStyle';
 import { theme } from './styles/theme';
+import PdfUpload from './components/pdfUpload';
+
+/*
+  [과제 항목]
+  1) 사용자가 PDF 파일을 업로드할 수 있어야 함
+  2) 업로드된 PDF 파일을 웹 페이지에서 볼 수 있어야 함
+  3) 뷰잉된 PDF파일에서 신구조문을 배열 형태로 파생하는 결과를 도출해야함 
+  (첨부파일 중에 이중 배열 형태의 결과파일 있음)
+*/
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">프로젝트</div>
+      <PdfUpload />
     </ThemeProvider>
   );
 }
