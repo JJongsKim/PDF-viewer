964d547dfcb880552f04c1ced3702346e761aad3
JJongsKim <totnwjd530@gmail.com>
2024-06-16 12:53:35 +0900

    Style: Pdf 뷰어 컴포넌트 틀 잡기

---
+++ src/components/pdfViewer/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './style';
+
+const PdfViewer = () => {
+  return (
+    <S.PreviewWrap>
+      <S.PreviewLabel>미리보기</S.PreviewLabel>
+      <S.PreviewBox />
+    </S.PreviewWrap>
+  );
+};
+
+export default PdfViewer;
---
+++ src/components/pdfViewer/style.ts
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+
+const PreviewWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PreviewLabel = styled.label`
+  font-size: 0.9rem;
+  font-weight: 600;
+  margin: 5px 0;
+`;
+
+const PreviewBox = styled.div`
+  width: 500px;
+  height: 600px;
+  border: 1px solid ${({ theme }) => theme.colors.light_gray};
+`;
+
+export { PreviewWrap, PreviewLabel, PreviewBox };
